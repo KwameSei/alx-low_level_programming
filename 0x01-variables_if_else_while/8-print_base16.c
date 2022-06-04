d0e441818fe66e95d3eadf2b41090226c185f381
KwameSei <nathanielosei92@gmail.com>
2022-06-04 05:50:54 +0000

    Looping through alphanumeric characters in hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch;
+
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'a'; ch <= 'e'; ch++)
+{
+putchar(ch);
+
+}
+putchar('\n');
+return (0);
+}
