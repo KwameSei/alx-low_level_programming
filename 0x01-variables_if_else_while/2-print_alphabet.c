e8861c1a66e8f139fc831cb7665f8d34e64114c3
KwameSei <nathanielosei92@gmail.com>
2022-06-04 03:31:01 +0000

    Printing characters with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch = 'a';
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+
+return(0);
+}
