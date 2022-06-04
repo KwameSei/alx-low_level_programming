c27bbab8f6c72cba423e83897ae8bea625ba4ef0
KwameSei <nathanielosei92@gmail.com>
2022-06-04 12:09:08 +0000

    Printing to standard output using puts edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,15 @@
 
 /* betty style doc for function main goes there */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 
 return (0);
 }
