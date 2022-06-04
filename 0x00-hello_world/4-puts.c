a700a650e7a108af3f66465b1375a342db1d789e
KwameSei <nathanielosei92@gmail.com>
2022-06-04 11:40:35 +0000

    Printing to standard output using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+
+return (0);
+}
