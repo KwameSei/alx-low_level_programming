e8763ec843aeaf0553226c3664c0c8c45acb5e81
KwameSei <nathanielosei92@gmail.com>
2022-06-08 16:00:22 +0000

    Printing every minute of the day

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_abs - checks for absolute value
+ *@n: character to check
+ *
+ *Description: Prints n absolute number.
+ *Return: n
+ */
+
+int print_last_digit(int n)
+{
+int d;
+ 
+d = n/10;
+
+return (0);
+}
