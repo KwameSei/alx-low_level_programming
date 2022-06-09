67ca03b1232afdabdb2482520f8ecde0b07370c7
KwameSei <nathanielosei92@gmail.com>
2022-06-09 20:17:41 +0000

    Creating uppercase letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - This function prints
+ * A string of characters using _putchar
+ * Return: 0 or 1.
+ */
+
+int _isupper(int c)
+{
+ 
+  if (c >= 'A' && c <= 'Z')
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
+      
+}
