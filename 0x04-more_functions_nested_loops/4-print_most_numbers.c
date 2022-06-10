37f50b2204bd4800957d2ee66a99c7ed55188f79
KwameSei <nathanielosei92@gmail.com>
2022-06-10 04:25:38 +0000

    Display digits 0 t0 9 without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_most_numbers - This function prints
+* Numbers from 0 to 9 excerpt 2 and 4
+*@n: connecting character
+* Return: 0.
+*/
+
+void print_most_numbers(void)
+{
+int n;
+
+for (n = '0'; n <= '9'; n++)
+{
+if (n == '2' || n == '4')
+{
+continue;
+}
+else
+{
+_putchar(n);
+}
+}
+_putchar('\n');
+}
