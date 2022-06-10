cb06e12938ffbff292b2467b2ab5d3dc7e788b27
KwameSei <nathanielosei92@gmail.com>
2022-06-10 14:26:59 +0000

    Fizz Buzz Game

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - This function prints
+* Fizz Buzz game
+*@n: connecting character 1
+* Return: 0.
+*/
+int main(void)
+{
+int n;
+
+for (n = 1; n <= 100; n++)
+{
+if (n % 15 == 0)
+{
+printf("FizzBuzz ");
+}
+if (n % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (n % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", n);
+}
+}
+printf("\n");
+
+return (0);
+}
