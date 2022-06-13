c2fe9ea9f57dbe9b636dde4bb712fa072d9aa9b5
KwameSei <nathanielosei92@gmail.com>
2022-06-13 19:12:21 +0000

    Function that swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - This function prints
+ * Multiplies two numbers
+ *@a: connecting character 1
+ *@b: connecting character 2
+ * Return: 0.
+ */
+void swap_int(int *a, int *b)
+{
+int num;
+num = *a;
+*a = *b;
+*b = num;
+}
