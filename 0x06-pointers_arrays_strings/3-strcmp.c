28dca6de1f7ef601e709bc0667e59d03f0c85fcd
KwameSei <nathanielosei92@gmail.com>
2022-06-16 16:55:39 +0000

    Function that compares two strings edited

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,19 +9,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int a = 0;
-
-while (s1[a] == s2[a])
-{
-if (s1[a] == '\0' || s2[a] == '\0')
-break;
-a++;
-}
-
-if (s1[a] == '\0' && s2[a] == '\0')
+  int a;
+  for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
+    {
+      if (s1[a] != s2[a])
+	return (s1[a] - s2[a]);
+    }
 return (0);
-else if (s2[a] > s1[a])
-return (-15);
-else
-return (15);
 }
