5c70ab859a5583b4aa8bfd60372e63f44a890781
KwameSei <nathanielosei92@gmail.com>
2022-06-22 18:28:59 +0000

    Printing string with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - This function prints
+ * Multiplies two numbers
+ *@s: connecting character 1
+ * Return: 0.
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0') /* null character */
+return;
+puts(s);
+_puts_recursion(s - 1);
+}
