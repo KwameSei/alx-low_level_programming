2f7e3b926e45db5b30322ac91b6af83a6e3ab5ef
KwameSei <nathanielosei92@gmail.com>
2022-06-24 16:43:58 +0000

    Printing the number of command line arguments using argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - This function prints
+ * the name of the executable file
+ *@argc: connecting character 1
+ *@argv: connecting character 2
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+
+if (argv)
+{
+printf("%d\n", argc);
+}
+
+return (0);
+}
