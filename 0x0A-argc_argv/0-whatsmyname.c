ecc43acff0f267b3a22e93edd96d6faab0f0d2ef
KwameSei <nathanielosei92@gmail.com>
2022-06-24 14:09:20 +0000

    Printing the executable file name using argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - This function prints
+ * the name of the executable file
+ *@argc: connecting character 1
+ *@argv: connecting character 2
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+argc = 0;
+printf("%s\n", argv[0]);
+
+return (0);
+}
