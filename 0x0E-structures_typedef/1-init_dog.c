75e9511cbf10e8842ff69d46e25ed20e2ccbbc7b
KwameSei <nathanielosei92@gmail.com>
2022-07-05 21:56:30 +0000

    Initializing a variable with struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,1 +1,22 @@
-void init_dog(struct dog *d, char *name, float age, char *owner);
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialises a variable of type struct dog
+ * @d: address of variable
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ * Description: this creates object dog of the animal class
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,11 +9,22 @@
  * Description: this creates object dog of the animal class
  */
 
-typedef struct dog
+struct dog
 {
 char *name;
 float age;
 char *owner;
-} create_dog;
+};
+
+/**
+ * dog - typedef for dog struct
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
