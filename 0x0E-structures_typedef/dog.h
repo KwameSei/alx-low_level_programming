3973c71897e5b35469671cf36ad2d32273158ea8
KwameSei <nathanielosei92@gmail.com>
2022-07-05 20:07:13 +0000

    Struct object dog created edited

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct dog - this create object dog of the animal class
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
  *
- * Description: struct header file for the main file
+ * Description: this creates object dog of the animal class
  */
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} create_dog;
+
+#endif
