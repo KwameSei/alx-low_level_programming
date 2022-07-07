ca855eca4c0caa2ac33c1ec3a7c4ea4b1af853f8
KwameSei <nathanielosei92@gmail.com>
2022-07-07 09:22:50 +0000

    Function pointer that prints a name edited

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,6 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
 /**
  * print_name - This function pointer
  * prints name
@@ -8,5 +11,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+if (name && f)
 f(name);
 }
