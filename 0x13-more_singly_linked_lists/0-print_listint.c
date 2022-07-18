af2fade021f59505523c4f0430833cb327a93eec
KwameSei <nathanielosei92@gmail.com>
2022-07-18 13:25:53 +0000

    Displaying sinly linked integers

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list
+ * @h: the list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+size_t a = 0;
+
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+a++;
+}
+return (a);
+}
