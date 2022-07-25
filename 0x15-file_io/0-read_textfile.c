f18785c4b08f729eb06cf3ab7965da8aed2836f1
KwameSei <nathanielosei92@gmail.com>
2022-07-25 15:51:52 +0000

    reading a text file and printing it to the POSIX standard output edited

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,37 +16,38 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd, _read, _write;
-  char *buf;
+int fd, _read, _write;
+char *buf;
 
-  if (filename == NULL)
-    return (0);
+if (filename == NULL)
+return (0);
 
-  fd = open(filename, O_RDONLY);
-  if (fd == -1)
-    return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
 
-      buf = malloc(sizeof(char) * letters);
-  if (buf == NULL)
-    return (0);
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+return (0);
 
-  /* Reading File */
-  _read = read(fd, buf, letters);
-  if (_read == -1)
-    {
-      free(buf);
-      return (0);
-    }
+/* Reading File */
+_read = read(fd, buf, letters);
+if (_read == -1)
+{
+free(buf);
+return (0);
+}
 
-  /* Writing File */
-  _write = write(STDOUT_FILENO, buf, _read);
-  if (_write == -1 || _read != _write)
-    {
-      free(buf);
-      return (0);
-    }
-  free(buf);
-  close(fd);
+/* Writing File */
+_write = write(STDOUT_FILENO, buf, _read);
+if (_write == -1 || _read != _write)
+{
+free(buf);
+return (0);
+}
 
-  return (_write);
+free(buf);
+close(fd);
+
+return (_write);
 }
