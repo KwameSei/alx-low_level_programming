b390b88add31906f20ff70ea9e201ed00ab069ad
KwameSei <nathanielosei92@gmail.com>
2022-09-23 12:54:48 +0000

    Function that retrieves a value associated with a key edited

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,14 +16,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (key == NULL || ht == NULL)
 		return (NULL);
 
-  	index = key_index((const unsigned char *)key, ht->size);
-  	nodo = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	nodo = ht->array[index];
 
-  	for (; nodo != NULL; nodo = nodo->next)
+	for (; nodo != NULL; nodo = nodo->next)
 	{
 		if (strcmp(nodo->key, key) == 0)
 			return (nodo->value);
 	}
 
-  	return (NULL);
+	return (NULL);
 }
