32f8c340bc18fa0c194cae2d5917cb4c20aa0dbc
KwameSei <nathanielosei92@gmail.com>
2023-01-05 14:06:25 +0000

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,7 +7,9 @@
  * @size: The number of elements in the array.
  * @value: = The target to search for.
  *
- * Return: If the value is not present or the array is NULL, -1.
+ * Return: If the target is not present or the array is NULL, -1.
+ *
+ * Description: Prints a value every time it is compared in the array.
  */
 
 int linear_search(int *array, size_t size, int value)
